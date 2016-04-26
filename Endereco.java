93ea48441ecba46b7d38599f227bd43c260de364
ubrals <deadkenn@uol.com.br>
2016-04-26 12:43:18 -0300

    Added files via upload

---
+++ Endereco.java
@@ -0,0 +1,5 @@
+package principais;
+
+public abstract class Endereco {
+    
+}
---
+++ Entidade.java
@@ -0,0 +1,19 @@
+package principais;
+
+public class Entidade {
+    private String nome;
+
+    public Entidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
